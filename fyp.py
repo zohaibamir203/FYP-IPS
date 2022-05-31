0c9d39697040cef47b8572fe4defe5f0bb200f52
zohaibamir203 <zohaibamir203@gmail.com>
2022-06-01 02:20:27 +0500

    Same and Different Floor Working. First Floor need to have a Purple Route too.

--- fyp.py
+++ fyp.py
@@ -263,22 +263,39 @@ def lift(loc,dest):
     path =[]
 
     # Ground Floor Shop Array
-    GroundShop = ['G-34','G-35','G-36','G-33','G-37','G-38','Lift','G-39','G-31','G-40','G-30','G-29','G-28','G-27','G-01','G-02','G-26',
-    'G-03','G-25','G-04','G-05','G-24','G-06','G-23','G-07','G-08','G-09','G-11A','G-11','G-12','G-22','G-13','G-21','G-14','G-20','G-19',
-    'G-15','G-18','G-17','G-16']
-
+    GroundFShop = ['G-34','G-35','G-36','G-33','G-37','G-38','Lift','G-39','G-31','G-40','G-30','G-40A','G-29','G-28','G-27','G-01','G-02','G-26',
+    'G-03','G-25','G-04','G-05','G-24','G-06','G-23','G-07','G-08','G-09','G-11A','G-11','G-22A','G-12','G-22','G-13','G-21','G-14',
+    'G-20','G-19','G-15','G-18','G-17','G-16']
+    GroundSShop = ['G-41','G-42','EMPTY','EMPTY','G-43','EMPTY','EMPTY','G-05','G-24','G-06','G-23','G-07','G-08','G-09','G-11A','G-11',
+    'G-22A','G-12','G-22','G-13','G-21','G-14','G-20','G-19','G-15','G-18','G-17','G-16']
+    GroundTShop = ['G-04','G-25','G-03','G-26','G-02','G-01','G-27','G-28','EMPTY','G-41','G-42','EMPTY','EMPTY','G-43']
+    GroundRShop = ['G-34','G-35','G-36','G-33','G-37','G-38','Lift','G-39','G-31','G-40','G-30','G-40A','G-29','EMPTY','G-41','G-42',
+    'EMPTY','EMPTY','G-43']
     # Ground Floor Shop Pixels Array
-    GroundPixels = [[110,75],[132,75],[147,75],[162,75],[164,75],[183,75],[188,75],[198,75],[202,75],[219,75],[233,75],[264,75],[293,75],[318,75],[354,75],[354,115],[354,122],
-    [354,148],[354,174],[354,188],[354,308],[354,328],[354,350],[354,380],[354,386],[354,429],[316,429],[293,429],[266,429],[240,429],[218,429],[202,429],[198,429],[189,429],[181,429],[164,429],
-    [161,429],[147,429],[131,429],[110,429]]
-    
+    GroundFPixels = [[110,75],[132,75],[147,75],[162,75],[164,75],[183,75],[188,75],[198,75],[202,75],[219,75],[233,75],[251,75],[264,75],[293,75],[318,75],[354,75],[354,115],[354,122],
+    [354,148],[354,174],[354,188],[354,308],[354,328],[354,350],[354,380],[354,386],[354,429],[316,429],[293,429],[266,429],[251,429],[240,429],[218,429],[202,429],[198,429],[189,429],
+    [181,429],[164,429],[161,429],[147,429],[131,429],[110,429]]
+    GroundSPixels = [[282,115],[282,167],[282,212],[212,212],[212,250],[212,291],[354,291],[354,308],[354,328],[354,350],[354,380],[354,386],[354,429],[316,429],[293,429],[266,429],
+    [251,429],[240,429],[218,429],[202,429],[198,429],[189,429],[181,429],[164,429],[161,429],[147,429],[131,429],[110,429]]
+    GroundTPixels = [[354,188],[354,174],[354,148],[354,122],[354,115],[354,75],[318,75],[293,75],[282,75],[282,115],[282,167],[282,212],[212,212],[212,250]]
+    GroundRPixels = [[110,75],[132,75],[147,75],[162,75],[164,75],[183,75],[188,75],[198,75],[202,75],[219,75],[233,75],[251,75],[264,75],[282,75],[282,115],[282,167],
+    [282,212],[212,212],[212,250]]
      # First Floor Shop Array
-    FirstShop = ['F-40','Lift','F-38','F-37','F-32','F-36','F-31','F-35','F-30','F-34','F-29','F-01','F-02','F-03','F-04','F-05','F-06','F-07',
-    'F-08','F-09','F-28','F-10','F-11','F-12','F-27','F-13','F-26','F-14','F-25','F-15','F-24','F-16','F-17','F-18','F-20','F-21','F-22','F-23']
-
+    FirstFShop = ['F-41','F-40','Lift','F-38','F-33','F-37','F-32','F-36','F-31','F-35','F-30','F-34','F-01',
+    'F-02','F-03','F-29','F-04','F-05','F-06','F-07','F-08','F-09','F-28','F-10','F-11','F-12','F-27',
+    'F-13','F-26','F-14','F-25','F-15','F-24','F-16','F-17','F-18','F-19']
+    FirstSShop = ['F-41','F-40','Lift','EMPTY','F-23','F-22','F-21','F-20','F-17','F-18','F-19']
+    FirstTShop = ['F-20','F-21','F-22','F-23','EMPTY','F-38','F-33','F-37','F-32','F-36','F-31','F-35','F-30','F-34','F-01',
+    'F-02','F-03','F-29','F-04','F-05','F-06','F-07','F-08','F-09','F-28','F-10','F-11','F-12','F-27','F-13','F-26','F-14',
+    'F-25','F-15','F-24','F-16','F-17','F-20']
     # First Floor Shop Pixel Array
-    FirstPixels = [[162,82],[182,82],[201,82],[230,82],[252,82],[268,82],[283,82],[293,82],[313,82],[317,82],[357,82],[357,112],[357,146],[357,148],[357,182],[357,217],[357,248],[357,283],
-    [357,317],[357,312],[357,356],[357,386],[357,421],[318,421],[316,421],[294,421],[282,421],[267,421],[250,421],[234,421],[215,421],[201,421],[185,421],[162,421],[187,371],[187,284],[187,200],[187,132]]
+    FirstFPixels = [[110,82],[162,82],[182,82],[201,82],[218,82],[230,82],[252,82],[268,82],[283,82],[293,82],[313,82],[317,82],[357,82],
+    [357,112],[357,146],[357,148],[357,182],[357,217],[357,248],[357,283],[357,317],[357,312],[357,356],[357,386],[357,421],[318,421],[316,421],
+    [294,421],[282,421],[267,421],[250,421],[234,421],[215,421],[201,421],[185,421],[162,421],[110,421]]
+    FirstSPixels = [[110,82],[162,82],[182,82],[186,82],[186,132],[186,200],[186,284],[186,372],[186,421],[162,421],[110,421]]
+    FirstTPixels = [[186,372],[186,284],[186,200],[186,132],[186,82],[201,82],[218,82],[230,82],[252,82],[268,82],[283,82],[293,82],[313,82],[317,82],[357,82],
+    [357,112],[357,146],[357,148],[357,182],[357,217],[357,248],[357,283],[357,317],[357,312],[357,356],[357,386],[357,421],[318,421],[316,421],
+    [294,421],[282,421],[267,421],[250,421],[234,421],[215,421],[201,421],[185,421],[186,372]]
 
     # Mark the Location and Destination Shop
     def MarkCurrLoc(pixelValue):
@@ -335,33 +352,82 @@ def lift(loc,dest):
         for i in range(len(path)):
             if(i!=len(path)-1):
                 MarkLine(path[i],path[i+1])
+    
     if (str(loc)[0] == 'G') or (str(dest)[0] == 'G'):
         img = cv2.imread("GFloor.png")
         if (str(loc)[0] == 'G'):
-            StartPos = GroundShop.index(str(loc))
+            if (str(loc) in GroundFShop):
+                StartPos = GroundFShop.index(str(loc))
+                EndPos = GroundFShop.index('Lift')
+                ShopPixel = GroundFPixels[StartPos]
+                LiftPixel = GroundFPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,GroundFPixels)
+            else:
+                StartPos = GroundRShop.index(str(loc))
+                EndPos = GroundRShop.index('Lift')
+                ShopPixel = GroundRPixels[StartPos]
+                LiftPixel = GroundRPixels[EndPos] 
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,GroundRPixels)   
         elif(str(dest)[0] == 'G'):
-            StartPos = GroundShop.index(str(dest))
-        EndPos = GroundShop.index('Lift')
-        ShopPixel = GroundPixels[StartPos]
-        LiftPixel = GroundPixels[EndPos]
-        MarkCurrLoc(ShopPixel)
-        MarkCurrLoc(LiftPixel)
-        CreatePath (StartPos,EndPos,GroundPixels)
+            if (str(dest) in GroundFShop):
+                StartPos = GroundFShop.index(str(dest))
+                EndPos = GroundFShop.index('Lift')
+                ShopPixel = GroundFPixels[StartPos]
+                LiftPixel = GroundFPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,GroundFPixels)
+            else:
+                StartPos = GroundRShop.index(str(dest))
+                EndPos = GroundRShop.index('Lift')
+                ShopPixel = GroundRPixels[StartPos]
+                LiftPixel = GroundRPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,GroundRPixels)
         cv2.imwrite("GroundFloorLift.png",img)
         path = []
 
     if (str(loc)[0] == 'F') or (str(dest)[0] == 'F'):
         img = cv2.imread("FFloor.png")
         if (str(loc)[0] == 'F'):
-            StartPos = FirstShop.index(str(loc))
+            if (str(loc) in FirstFShop):
+                StartPos = FirstFShop.index(str(loc))
+                EndPos = FirstFShop.index('Lift')
+                ShopPixel = FirstFPixels[StartPos]
+                LiftPixel = FirstFPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,FirstFPixels)
+            else:
+                StartPos = FirstSShop.index(str(loc))
+                EndPos = FirstSShop.index('Lift')
+                ShopPixel = FirstSPixels[StartPos]
+                LiftPixel = FirstSPixels[EndPos] 
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,FirstSPixels)   
         elif(str(dest)[0] == 'F'):
-            StartPos = FirstShop.index(str(dest))
-        EndPos = FirstShop.index('Lift')
-        ShopPixel = FirstPixels[StartPos]
-        LiftPixel = FirstPixels[EndPos]
-        MarkCurrLoc(ShopPixel)
-        MarkCurrLoc(LiftPixel)
-        CreatePath (StartPos,EndPos,FirstPixels)
+            if (str(dest) in FirstFShop):
+                StartPos = FirstFShop.index(str(dest))
+                EndPos = FirstFShop.index('Lift')
+                ShopPixel = FirstFPixels[StartPos]
+                LiftPixel = FirstFPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,FirstFPixels)
+            else:
+                StartPos = FirstSShop.index(str(dest))
+                EndPos = FirstSShop.index('Lift')
+                ShopPixel = FirstSPixels[StartPos]
+                LiftPixel = FirstSPixels[EndPos]
+                MarkCurrLoc(ShopPixel)
+                MarkCurrLoc(LiftPixel)
+                CreatePath (StartPos,EndPos,FirstSPixels)
         cv2.imwrite("FirstFloorLift.png",img)
         path = [] 
 
